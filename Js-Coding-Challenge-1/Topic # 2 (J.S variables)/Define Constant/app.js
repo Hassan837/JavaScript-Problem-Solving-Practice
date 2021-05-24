35991a4b8471fc1490ae7b5036fc900fa05ad227
Hassan <hassanirfan429@gmail.com>
2021-05-25 02:21:15 +0500

    using const variable

---
+++ Js-Coding-Challenge-1/Topic # 2 (J.S variables)/Define Constant/app.js
@@ -0,0 +1,26 @@
+
+// this fuction is for numeric
+function numeric(){
+ 
+    const a =24;
+    document.querySelector("#num").innerHTML = a;
+
+}
+
+
+// this fuction is for string
+function string(){
+ 
+    let a ="Muhammad Hassan Irfan";
+    document.querySelector("#str").innerHTML = a;
+
+}
+
+// this function is for string
+function boolean(){
+ 
+    let isPass = 10 > 9;
+    document.querySelector(".boo").innerHTML = isPass;
+
+    
+}
