fddc3a2b2d41d4d8efeb3ab6077458013df63d44
Hassan <hassanirfan429@gmail.com>
2021-05-25 00:30:02 +0500

    Externa js button

---
+++ Js-Coding-Challenge-1/Topic # 1 (Adding scripts)/External-JS-Button/app.js
@@ -0,0 +1,19 @@
+function red(){
+ 
+    alert('This is the alert for button_Red button');
+
+}
+
+
+function blue(){
+ 
+    alert('This is the alert for button_Blue button');
+
+}
+
+
+function green(){
+ 
+    alert('This is the alert for button_blue button');
+
+}
