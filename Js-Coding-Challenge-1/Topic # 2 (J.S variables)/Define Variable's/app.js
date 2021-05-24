511712993e86a101133ba31e203a0d9b355587ad
Hassan <hassanirfan429@gmail.com>
2021-05-25 02:04:52 +0500

    using boolean expression

--- Js-Coding-Challenge-1/Topic # 2 (J.S variables)/Define Variable's/app.js
+++ Js-Coding-Challenge-1/Topic # 2 (J.S variables)/Define Variable's/app.js
@@ -1,3 +1,26 @@
-function External(){
-    alert('This External J.S');
+
+// this fuction is for numeric
+function numeric(){
+ 
+    let a =10;
+    document.querySelector("#num").innerHTML = a;
+
+}
+
+
+// this fuction is for string
+function string(){
+ 
+    let a ="Muhammad Hassan Irfan";
+    document.querySelector("#str").innerHTML = a;
+
+}
+
+// this function is for string
+function boolean(){
+ 
+    let isPass = 10 > 9;
+    document.querySelector(".boo").innerHTML = isPass;
+
+    
 }
