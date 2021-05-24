db1c579b141c0aff9471f9d46bee75acd93f5832
Hassan <hassanirfan429@gmail.com>
2021-05-25 03:12:35 +0500

    DOM work done

---
+++ Js-Coding-Challenge-1/Topic # 2 (J.S variables)/DocumentObjectModel/app.js
@@ -0,0 +1,9 @@
+function show(){
+    
+    let inputValue = document.querySelector('#inp1').value;
+
+    alert('Hello' + ' '  + inputValue + ' ' +'The message has been sent on the given number');
+
+
+    
+}
