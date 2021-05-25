0bcea629e20f6e4a53df7dbdb1a5900051999331
Hassan <hassanirfan429@gmail.com>
2021-05-25 23:22:00 +0500

    comparission operator task done

---
+++ Js-Coding-Challenge-1/Topic # 3 (Operators)/2. Comparision operator/app.js
@@ -0,0 +1,52 @@
+// Comparission operators 
+
+var  a = 5, b= 10, c= "5";
+var x = a;
+
+document.write(a == c);
+
+document.write("<br><br>");
+
+document.write(a === c);
+
+document.write("<br><br>");
+
+document.write(a == x);
+
+document.write("<br><br>");
+
+document.write(a != b);
+
+document.write("<br><br>");
+
+document.write(a > b);
+
+document.write("<br><br>");
+
+document.write(a < b);
+
+document.write("<br><br>");
+
+document.write(a >= b);
+
+document.write("<br><br>");
+
+document.write(a <= b);
+
+document.write("<br><br>");
+
+document.write(a >= c);
+
+document.write("<br><br>");
+
+document.write(a <= c);
+
+document.write("<br><br>");
+
+
+
+
+
+
+
+
