c3f83d606368deaa0ea451a1347010409eb028ad
Hassan <hassanirfan429@gmail.com>
2021-05-25 23:06:35 +0500

    js operator task done

---
+++ Js-Coding-Challenge-1/Topic # 3 (Operators)/1. arthematic operator/app.js
@@ -0,0 +1,31 @@
+// operators 
+
+var  x = 5, y= 10, z= 15;
+
+document.write(x + y);
+
+document.write("<br><br>");
+
+document.write(y - x);
+
+document.write("<br><br>");
+
+document.write(x * y);
+
+document.write("<br><br>");
+
+document.write(y / x);
+
+document.write("<br><br>");
+
+document.write(x % 2);
+
+document.write("<br><br>");
+
+document.write(x++);
+
+document.write("<br><br>");
+
+document.write(x--);
+
+document.write("<br><br>");
