fcc1fb946e7f2cf15a28c7475c63f15783076bea
Hassan <hassanirfan429@gmail.com>
2021-06-01 00:06:03 +0500

    data type practice

---
+++ JS-Coding Task#1/Topic  (Datatypes)/Problem (checkIt)/app.js
@@ -0,0 +1,25 @@
+// checking type of integer
+
+let a = 5 ;
+console.log(a);
+
+// checking type of decimal number
+
+let b = 23.5;
+console.log(b);
+
+// checking type of boolean type
+
+let c = (10 > 9);
+console.log(c);
+
+// checking the  type of string value 
+
+let d = "55", e = 55;
+console.log(d);
+console.log(e);
+
+// checking the type of empty variable
+
+let f;
+console.log(f);
