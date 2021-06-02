1537cdcc2743b160a2ed4f420903f54cfdb8e877
Hassan <hassanirfan429@gmail.com>
2021-06-03 00:27:52 +0500

    problem logical operator done

---
+++ JS - Coding Task # 3/Topic Operators/3. Problem  (LogicalOperator)/app.js
@@ -0,0 +1,24 @@
+True && True
+// Answer is TRUE
+
+False && True
+// Answer is FALSE
+
+False && False
+// Answer is FALSE
+
+
+False || True
+// Answer is TRUE
+
+
+5>4 && 2<5
+// Answer is TRUE
+
+
+2==9 || 2==2
+// Answer is TRUE
+
+
+!(!(4>2))
+// Answer is TRUE
