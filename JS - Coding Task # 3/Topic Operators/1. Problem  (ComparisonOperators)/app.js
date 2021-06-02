ea9501b6fb7cac430a3971c86d3d0b211056e3ed
Hassan <hassanirfan429@gmail.com>
2021-06-02 23:44:13 +0500

    problem comparision operators done

---
+++ JS - Coding Task # 3/Topic Operators/1. Problem  (ComparisonOperators)/app.js
@@ -0,0 +1,25 @@
+let a = 4;
+let b = 5;
+
+let check = a <= b;
+
+console.log(check);
+
+
+//list of comparision Operators are
+
+// 1. == equal to
+
+//2. === equal value and equal type
+
+//3. != not equal 
+
+//4. !== not equal value or not equal type
+
+//5. >	greater than
+
+//6. <	less than
+
+//7. >=	greater than or equal to
+
+//8. <=	less than or equal to
