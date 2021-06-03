faf9a73c53561bc2a14c02962cbe48147628fabd
Hassan <hassanirfan429@gmail.com>
2021-06-03 21:34:07 +0500

    function calling with diff parameters

---
+++ JS - Coding Task # 5/Topic  (FUNCTION)/2. Problem Name (FunctionCalling)/app.js
@@ -0,0 +1,37 @@
+// calling the funtion multiples time with
+// different parameters.
+
+function double(paisa) {
+    let number = paisa * 2;
+
+    return number;
+}
+
+console.log(double(2));
+
+//2nd time
+function double(rupees) {
+    let number = rupees * 2;
+
+    return number;
+}
+
+console.log(double(4));
+
+// 3rd time
+function double(amount) {
+    let number = amount * 2;
+
+    return number;
+}
+
+console.log(double(6));
+
+//4th time
+function double($) {
+    let number = $ * 2;
+
+    return number;
+}
+
+console.log(double(8));
