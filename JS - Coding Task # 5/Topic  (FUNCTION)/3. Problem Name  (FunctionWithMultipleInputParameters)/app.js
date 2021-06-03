9c192f3e3f3f9668d68c54444be12e71c468919f
Hassan <hassanirfan429@gmail.com>
2021-06-03 21:43:49 +0500

    FunctionWithMultipleInputParameters

---
+++ JS - Coding Task # 5/Topic  (FUNCTION)/3. Problem Name  (FunctionWithMultipleInputParameters)/app.js
@@ -0,0 +1,11 @@
+// calling the funtion with multiple input 
+//  parameters.
+
+function addTwonumbers(Num1,Num2) {
+    let sum = Num1 + Num2;
+
+    return sum;
+}
+
+console.log(addTwonumbers(10,20));
+
